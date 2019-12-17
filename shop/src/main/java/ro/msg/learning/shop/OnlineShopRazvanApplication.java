e71bcd7da5ad4e7d8d2d7e4fa61651c92ba65789
Razvan-Daniel Mihaescu <razvan.mihaescu@msg.group>
2019-12-17 11:30:48 +0200

    Initial commit

---
+++ shop/src/main/java/ro/msg/learning/shop/OnlineShopRazvanApplication.java
@@ -0,0 +1,13 @@
+package ro.msg.learning.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopRazvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopRazvanApplication.class, args);
+	}
+
+}
---
+++ shop/src/test/java/ro/msg/learning/shop/OnlineShopRazvanApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.msg.learning.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopRazvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
